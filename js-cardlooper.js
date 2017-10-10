ff4e6b713d8a981a7f6317c2b923ad8ac86d19b1
CosmicCandi <kalea.wolff@gmail.com>
2017-10-10 13:10:37 -0700

    Updates to use arrow functionality and const/let

--- js-cardlooper.js
+++ js-cardlooper.js
@@ -1,10 +1,10 @@
-var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
-var randomSuit = "";
+const suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+let randomSuit = "";
 
 //Generate a random number
 //Assign that random number to a suit
-var pickRandomSuit = function () {
-var random = Math.random();
+let pickRandomSuit = () => {
+  let random = Math.random();
   if (random <= 0.25) {
     randomSuit = suits[0];
 } else if (random <= 0.50) {
@@ -20,7 +20,8 @@ var random = Math.random();
 do {
   console.log(randomSuit);
   pickRandomSuit();
-  //Console Log "Clubs" when "Clubs" is finally drawn
+  
+//Console Log "Clubs" when "Clubs" is finally drawn
   if (randomSuit === suits[3]) {
     console.log(randomSuit);
   }
